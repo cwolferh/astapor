dac7a0957024707b06a3f81a9a9ae59064c7c64d
Crag Wolfe <cwolfe@redhat.com>
2013-10-21 20:08:55 -0400

    contribution from jguiditta, add $hamysql_active_node fact to determine when to add openstack accounts

--- puppet/modules/quickstack/manifests/hamysql/node.pp
+++ puppet/modules/quickstack/manifests/hamysql/node.pp
@@ -121,53 +121,12 @@ class quickstack::hamysql::node (
       onlyif => "/tmp/are-we-running-mysql.bash",
    }
 
-  # TODO use IP other than 127.0.0.1 if $mysql_bind_address is not 0.0.0.0
-  database { $keystone_db_dbname:
-    ensure => 'present',
-    provider => 'mysql',
-    require => Class['quickstack::hamysql::mysql::rootpw'],
-  }
-  database_user { "$keystone_db_user@127.0.0.1":
-    ensure => 'present',
-    password_hash => mysql_password($keystone_db_password),
-    provider      => 'mysql',
-    require       => Database[$keystone_db_dbname],
-  }
-
-  database { $glance_db_dbname:
-    ensure => 'present',
-    provider => 'mysql',
-    require => Class['quickstack::hamysql::mysql::rootpw'],
-  }
-  database_user { "$glance_db_user@127.0.0.1":
-    ensure => 'present',
-    password_hash => mysql_password($glance_db_password),
-    provider      => 'mysql',
-    require       => Database[$glance_db_dbname],
-  }
-
-  database { $nova_db_dbname:
-    ensure => 'present',
-    provider => 'mysql',
-    require => Class['quickstack::hamysql::mysql::rootpw'],
-  }
-  database_user { "$nova_db_user@127.0.0.1":
-    ensure => 'present',
-    password_hash => mysql_password($nova_db_password),
-    provider      => 'mysql',
-    require       => Database[$nova_db_dbname],
-  }
-
-  database { $cinder_db_dbname:
-    ensure => 'present',
-    provider => 'mysql',
-    require => Class['quickstack::hamysql::mysql::rootpw'],
-  }
-  database_user { "$cinder_db_user@127.0.0.1":
-    ensure => 'present',
-    password_hash => mysql_password($cinder_db_password),
-    provider      => 'mysql',
-    require       => Database[$cinder_db_dbname],
-  }
+   class {'quickstack::hamysql::mysql::setup':
+      keystone_db_password => $keystone_db_password,
+      glance_db_password   => $glance_db_password,
+      nova_db_password     => $nova_db_password,
+      cinder_db_password   => $cinder_db_password,
+      require              => Class['quickstack::hamysql::mysql::rootpw'],
+   }
 
 }
