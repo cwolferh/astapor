a0e0140aa70cbbd70fdb3106b00b68c5c7ef45ba
Crag Wolfe <cwolfe@redhat.com>
2015-01-17 00:24:37 -0500

    BZ #1182811 - create cloned resources in two steps (workaround)
    https://bugzilla.redhat.com/show_bug.cgi?id=1182811
    
    Create cloned resources with two commands to pcs.

--- puppet/modules/quickstack/manifests/pacemaker/resource/service.pp
+++ puppet/modules/quickstack/manifests/pacemaker/resource/service.pp
@@ -7,22 +7,40 @@ define quickstack::pacemaker::resource::service($group='',
   include quickstack::pacemaker::params
 
   if has_interface_with("ipaddress", map_params("cluster_control_ip")){
-    ::pacemaker::resource::service{ "$name":
+    ::pacemaker::resource::service{ "$title":
                                 group          => $group,
-                                clone          => $clone,
+                                clone          => false,
                                 interval       => $interval,
                                 monitor_params => $monitor_params,
                                 ensure         => $ensure,
                                 options        => $options}
 
-    anchor { "qprs start $name": } 
-    -> Pcmk_Resource["$name"]
-    -> exec {"wait for pcmk_resource $name":
+    anchor { "qprs start $title": }
+    -> Pcmk_Resource["$title"]
+    -> exec {"wait for pcmk_resource $title":
         timeout   => 3600,
         tries     => 360,
         try_sleep => 10,
-        command   => "/usr/sbin/pcs resource show $name",
+        command   => "/usr/sbin/pcs resource show $title",
     }
-    -> anchor { "qprs end $name": }
+    -> anchor { "qprs end $title": }
+
+    if $clone {
+      Exec["wait for pcmk_resource $title"] ->
+      exec {"create pcmk_resource $title clone":
+        command   => "/usr/sbin/pcs resource clone ${title}",
+        unless    => "/usr/sbin/pcs resource show ${title}-clone",
+        timeout   => 60,
+        tries     => 5,
+        try_sleep => 10,
+      } ->
+      exec {"wait for pcmk_resource ${title}-clone":
+        timeout   => 3600,
+        tries     => 360,
+        try_sleep => 10,
+        command   => "/usr/sbin/pcs resource show ${title}-clone",
+      } ->
+      Anchor["qprs end $title"]
+    }
   }
 }
